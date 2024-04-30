2c1c76da24e1db7b0f6c25aeca49888fcb1cb71e
okwhz <okwhz@126.com>
2024-04-30 15:42:37 +0800

    2024-04-30 15:41 增加了一个CMakeLists.native的文件，CMakeLists.txt中包含它就能编译native了。

--- config.h
+++ config.h
@@ -8,11 +8,11 @@
 #define VERSION_MINOR 01
 #define SOFT_VERSION "V1.01"
 #define GIT_PATH "git@github.com:okwhz/contiki-process.git"
-#define GIT_VERSION "df94340df7d1eaea3807ffc65eb037176d3accfa"
-#define GIT_COMMAND_ID "df94340"
+#define GIT_VERSION "3860b93b38ca1d82c78993002c3ae66d06bb4c9f"
+#define GIT_COMMAND_ID "3860b93"
 #define BRANCH_NAME "develop"
-#define COMPILE_TIME "20240430 15:20"
-#define COMMIT_DATE    "Tue Apr 30 11:43:11 2024"
-#define COMMIT_SUBJECT "8f637885a  (HEAD -> develop) Merge branch 'master' of https://github.com/contiki-ng/contiki-ng into develop 2024-04-26 16:52:01 +0800"
+#define COMPILE_TIME "20240430 15:37"
+#define COMMIT_DATE    "Tue Apr 30 15:27:02 2024"
+#define COMMIT_SUBJECT "2024-04-30 15:25 把native文件夹下的文件数量精简到了12个。准备在建立一个stm32的框架。"
 
 #endif
