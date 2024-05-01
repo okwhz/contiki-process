9c801e0a214dad5d015c8e5b36bf691f4fe1e0b6
okwhz <okwhz@126.com>
2024-05-01 12:55:19 +0800

    2024-05-01 12:55

--- config.h
+++ config.h
@@ -8,11 +8,11 @@
 #define VERSION_MINOR 01
 #define SOFT_VERSION "V1.01"
 #define GIT_PATH "git@github.com:okwhz/contiki-process.git"
-#define GIT_VERSION "2c1c76da24e1db7b0f6c25aeca49888fcb1cb71e"
-#define GIT_COMMAND_ID "2c1c76d"
+#define GIT_VERSION "06727b49c2aedd4e949d62be022e1925ecd252aa"
+#define GIT_COMMAND_ID "06727b4"
 #define BRANCH_NAME "develop"
-#define COMPILE_TIME "20240501 11:00"
-#define COMMIT_DATE    "Tue Apr 30 15:42:37 2024"
-#define COMMIT_SUBJECT "2024-04-30 15:41 增加了一个CMakeLists.native的文件，CMakeLists.txt中包含它就能编译native了。"
+#define COMPILE_TIME "20240501 12:14"
+#define COMMIT_DATE    "Wed May 1 11:04:46 2024"
+#define COMMIT_SUBJECT "2024-05-01 11:00 将对clock_time_t的长度定义宏放入了contiki-confi.h,更改了rtimer.c包含Contiki.h,否则timer.h没有引用contiki.h"
 
 #endif
