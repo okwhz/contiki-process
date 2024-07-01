55c726029fb89ddcd56f0bc7f78b08daa1df2ef3
okwhz <okwhz@126.com>
2024-07-01 17:08:28 +0800

    2024-07-01 17:08 viminfo

--- config.h
+++ config.h
@@ -8,11 +8,11 @@
 #define VERSION_MINOR 01
 #define SOFT_VERSION "V1.01"
 #define GIT_PATH "git@github.com:okwhz/contiki-process.git"
-#define GIT_VERSION "cf1795ebc32ead8d4acbfa8f67543f5c4431bf67"
-#define GIT_COMMAND_ID "cf1795e"
+#define GIT_VERSION "607a79f61aecaa968b6fe42c54f5da46b8030860"
+#define GIT_COMMAND_ID "607a79f"
 #define BRANCH_NAME "develop"
-#define COMPILE_TIME "20240701 16:32"
-#define COMMIT_DATE    "Mon Jul 1 07:45:36 2024"
-#define COMMIT_SUBJECT "2024-07-01 07:44 修改chdir、pipe没有返回值判断产生警告。"
+#define COMPILE_TIME "20240701 16:54"
+#define COMMIT_DATE    "Mon Jul 1 16:40:32 2024"
+#define COMMIT_SUBJECT "2024-07-01 16:38 改Cmakefile.lst文件，产生lst文件，减少文件大小。"
 
 #endif
